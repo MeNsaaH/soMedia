78259bc9bdc3f9a1750cae05c83611e53f7ec667
Manasseh Mmadu <mmadumanasseh@gmail.com>
2018-11-11 19:50:37 +0100

    Adding mobile compat

--- soMedia/settings.py
+++ soMedia/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '(gmgz&heyoh0o7ullp)&v_lyv!x9^z-0g=6%e*gcy0j@q&+q#w'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*',]
 
 
 # Application definition
@@ -120,6 +120,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, "libs", "static"),
 ]
